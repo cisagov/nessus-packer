62b7ff829c593d6bed0b5b9b8502d23d8ee2b92f
Dave Redmin <david.redmin@beta.dhs.gov>
2020-04-30 11:03:35 -0400

    Use a slightly less-restrictive pattern to account for double-digits in version strings (e.g. 8.10.0).

--- terraform-test-user/main.tf
+++ terraform-test-user/main.tf
@@ -54,7 +54,7 @@ module "third_party_bucket_read_staging" {
   iam_usernames = [module.iam_user.user.name]
   role_name     = "ThirdPartyBucketRead-${module.iam_user.user.name}"
   s3_bucket     = data.terraform_remote_state.images_staging.outputs.third_party_bucket.id
-  s3_objects    = ["Nessus-?.?.?-debian6_amd64.deb"]
+  s3_objects    = ["Nessus-*-debian6_amd64.deb"]
 }
 
 # Attach ThirdPartyBucketRead policy to the Staging EC2AMICreate role
