3cb7c40a06b759f26b40c78d7a4d9ad4e6b699d6
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-08-16 10:40:17 -0400

    Remove support for ARM64
    Nessus does not support ARM64 on Debian.

--- terraform-post-packer/main.tf
+++ terraform-post-packer/main.tf
@@ -24,49 +24,51 @@ locals {
   account_name_regex  = "^Shared Services \\(${local.images_account_type}\\)$|^env[[:digit:]]+ \\(${local.images_account_type}\\)$"
 }
 
+# Nessus does not support ARM64 on Debian.
 # The IDs of all ARM64 cisagov/nessus-packer AMIs
-data "aws_ami_ids" "historical_amis_arm64" {
-  owners = [data.aws_caller_identity.images.account_id]
+# data "aws_ami_ids" "historical_amis_arm64" {
+#   owners = [data.aws_caller_identity.images.account_id]
+#
+#   filter {
+#     name   = "architecture"
+#     values = ["arm64"]
+#   }
+#
+#   filter {
+#     name   = "name"
+#     values = ["nessus-hvm-*-arm64-ebs"]
+#   }
+#
+#   filter {
+#     name   = "root-device-type"
+#     values = ["ebs"]
+#   }
+#
+#   filter {
+#     name   = "virtualization-type"
+#     values = ["hvm"]
+#   }
+# }
 
-  filter {
-    name   = "architecture"
-    values = ["arm64"]
-  }
-
-  filter {
-    name   = "name"
-    values = ["nessus-hvm-*-arm64-ebs"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
+# Nessus does not support ARM64 on Debian.
 # Assign launch permissions to the ARM64 AMIs
-module "ami_launch_permission_arm64" {
-  # Really we only want the var.recent_ami_count most recent AMIs, but
-  # we have to cover the case where there are fewer than that many
-  # AMIs in existence.  Hence the min()/length() tomfoolery.
-  for_each = toset(slice(data.aws_ami_ids.historical_amis_arm64.ids, 0, min(var.recent_ami_count, length(data.aws_ami_ids.historical_amis_arm64.ids))))
-
-  source = "github.com/cisagov/ami-launch-permission-tf-module"
-
-  providers = {
-    aws        = aws
-    aws.master = aws.master
-  }
-
-  account_name_regex   = local.account_name_regex
-  ami_id               = each.value
-  extraorg_account_ids = var.extraorg_account_ids
-}
+# module "ami_launch_permission_arm64" {
+#   # Really we only want the var.recent_ami_count most recent AMIs, but
+#   # we have to cover the case where there are fewer than that many
+#   # AMIs in existence.  Hence the min()/length() tomfoolery.
+#   for_each = toset(slice(data.aws_ami_ids.historical_amis_arm64.ids, 0, min(var.recent_ami_count, length(data.aws_ami_ids.historical_amis_arm64.ids))))
+#
+#   source = "github.com/cisagov/ami-launch-permission-tf-module"
+#
+#   providers = {
+#     aws        = aws
+#     aws.master = aws.master
+#   }
+#
+#   account_name_regex   = local.account_name_regex
+#   ami_id               = each.value
+#   extraorg_account_ids = var.extraorg_account_ids
+# }
 
 # The IDs of all x86-64 cisagov/nessus-packer AMIs
 data "aws_ami_ids" "historical_amis_x86_64" {
--- terraform-post-packer/outputs.tf
+++ terraform-post-packer/outputs.tf
@@ -1,7 +1,8 @@
-output "launch_permissions_arm64" {
-  value       = module.ami_launch_permission_arm64
-  description = "The cisagov/ami-launch-permission-tf-module for each ARM64 AMI to which launch permission is being granted."
-}
+# Nessus does not support ARM64 on Debian.
+# output "launch_permissions_arm64" {
+#   value       = module.ami_launch_permission_arm64
+#   description = "The cisagov/ami-launch-permission-tf-module for each ARM64 AMI to which launch permission is being granted."
+# }
 
 output "launch_permissions_x86_64" {
   value       = module.ami_launch_permission_x86_64
