3588cf8d0fffc509d702abcd7de602630c304407
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-10-25 09:54:12 -0400

    Install cisagov/ansible-role-vnc-server
    This will add the vnc user and its associated ssh keys, which have
    been requested in cisagov/cool-system-internal#37.

--- terraform-build-user/main.tf
+++ terraform-build-user/main.tf
@@ -9,8 +9,15 @@ module "iam_user" {
     aws.images-staging-ssm    = aws.images-staging-ssm
   }
 
-  ssm_parameters = ["/cyhy/dev/users", "/ssh/public_keys/*"]
-  user_name      = "build-nessus-packer"
+  ssm_parameters = [
+    "/cyhy/dev/users",
+    "/ssh/public_keys/*",
+    "/vnc/password",
+    "/vnc/ssh/ed25519_private_key",
+    "/vnc/ssh/ed25519_public_key",
+    "/vnc/username",
+  ]
+  user_name = "build-nessus-packer"
 }
 
 # Attach Production ThirdPartyBucketRead policy to
