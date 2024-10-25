8896599d86145862721b7343fe0d2139dc36efa2
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-10-25 10:30:40 -0400

    Remove two unnecessary SSM Parameter Store parameters

--- terraform-build-user/main.tf
+++ terraform-build-user/main.tf
@@ -10,8 +10,6 @@ module "iam_user" {
   }
 
   ssm_parameters = [
-    "/cyhy/dev/users",
-    "/ssh/public_keys/*",
     "/vnc/password",
     "/vnc/ssh/ed25519_private_key",
     "/vnc/ssh/ed25519_public_key",
